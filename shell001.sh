89f03b15a6a3b63f65c012c974a720ba2d2b064d
kranthi318 <33656794+kranthi318@users.noreply.github.com>
2018-01-23 14:56:14 +0530

    Create shell001.sh

---
+++ shell001.sh
@@ -0,0 +1,4 @@
+ehco "this shell is exxcution off the process"
+echo "am preparing the data on git hub repo"
+echo -e "the are the studing subjects on devops \n1.LINUX\n2.GIT HUB\n3.CHEF\n4.ANSIBLE\n5.AWS\n6.JENKINS\n7.DOCKER\n8.MAVEN\n9.NAGIOS"
+echo "thank you ffor learninh"
